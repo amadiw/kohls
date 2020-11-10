2f7defc87a9213c4ade705a5151617926cf53419
Amadi Someswaran <amadi@someswaran.com>
2020-11-10 10:11:45 -0500

    chore: changed text and styling on popup div

--- kohls.js
+++ kohls.js
@@ -8,15 +8,9 @@ $(document).ready(() => {
 //triggers shopping cart link in order to have access to images in cart
     $("#tr_phase2_ShoppingBg").trigger("click")
 
-console.log('h2 ', $("h2"))
+
     let numItems = $(".number-items.boss-number-items.nonzero-items").text();
     let subtotal = $(".subtotal").text();
-
-    let hiddenPanel = $(
-      ".tr_phase2_headerPanel.tr_phase2_headerPanel_ghr.sliderInitDone"
-    );
-    console.log("hiddenPanel.... ", hiddenPanel);
-
     let images = [...$(".kas-newpb-product-image")].map((el) => el.src);
 
     console.log(
@@ -36,24 +30,20 @@ console.log('h2 ', $("h2"))
       position: "fixed",
       height: "300px",
       width: "600px",
-      background: "yellow",
+      background: "white",
       position: "absolute",
       top: "50%",
       left: "50%",
       transform: "translate(-50%, -50%)",
     });
-    console.log("hello from popup()");
-    console.log('subtotal... ', subtotal)
 
-    popupDiv.append(`<div> ${numItems} </div>`)
-    popupDiv.append(`<div> ${subtotal} </div>`)
-    console.log('image.... ',images[0] )
-//     popupDiv.append(`<img src="${images[0]}" width=25% height=25%/>`)
+    popupDiv.append(`<div id="numItems"> Items in cart:  ${numItems} </div>`)
+    popupDiv.append(`<div id="subtotal"> Subtotal:  ${subtotal} </div>`)
 
+    //renders cart images in popup div
     $.map(images, el => {
-    	popupDiv.append(`<img src="${el}" width=25% height=25% />`)
+    	popupDiv.append(`<img id="cartImage" src="${el}" width=25% height=25% />`)
     })
-
   };
 
 
@@ -66,24 +56,27 @@ console.log('h2 ', $("h2"))
       right: "0",
       bottom: "0",
       left: "0",
-      background: "black",
+      background: "rgba(0,0,0,.9)",
       display: "none",
-      opacity: ".97",
     });
 
     overlayDiv.appendTo(document.body);
+
+    //hides banners
     $(".hp2-creative").hide();
     $("#header").hide();
     $("#open-drawer").hide();
+
+
     $("#overlay").fadeIn();
   };
 
+  //trigger when user scrolls >=90% of page
   $(window).scroll(() => {
     if (
       $(window).scrollTop() / ($(document).height() - $(window).height()) >=
       0.9
     ) {
-
       overlay();
       popup();
     }
